1358f93fdf6d1e1f61172625cc0d31fc4e0ed6ce
richard-to <gohn76@gmail.com>
2012-11-05 12:33:54 -0900

    Fix lint errors in app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 var server = function(config){
-	var path = require('path')
-	var _ = require('underscore')
-	var express = require('express')
+	var path = require('path');
+	var _ = require('underscore');
+	var express = require('express');
 
-	var app = express()
+	var app = express();
 
-	app.engine('html', require('ejs').renderFile)
-	app.set('view', path.join(__dirname, 'views'))
-	app.set('view engine', 'html')
-	app.set('env', 'development')
+	app.engine('html', require('ejs').renderFile);
+	app.set('view', path.join(__dirname, 'views'));
+	app.set('view engine', 'html');
+	app.set('env', 'development');
 
-	app.use(express.static(path.join(__dirname, 'public')))
+	app.use(express.static(path.join(__dirname, 'public')));
 
 	app.get('/', function(req, res){
-		res.render('index') 
-	})
+		res.render('index'); 
+	});
 
-	return app
-}
+	return app;
+};
 
-var app = server()
-app.listen(5000)
+var app = server();
+app.listen(5000);
