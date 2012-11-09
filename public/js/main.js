4c1ce5ced65cc3d47af8a9210d4719922e5f9635
richard-to <gohn76@gmail.com>
2012-11-09 10:14:05 -0900

    Add basic ajax file upload functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,12 @@
 require(['lib/config'], function() {
-	require(['jquery.fileupload'], function(){
-	});
+    require(['jquery.fileupload'], function(){
+        $(function () {
+            $('#fileupload').fileupload({
+                dataType: 'json',
+                done: function (e, data) {
+                    $('<p/>').text(data.result.image).appendTo(document.body);
+                }
+            });
+        });   
+    });
 });
