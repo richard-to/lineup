ea53f9c485cc413fbd786a54b2a98b3d0971a5d0
richard-to <gohn76@gmail.com>
2012-11-10 12:27:57 -0900

    Move image clean up to Makefile instead of grunt task

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 test:
 	./node_modules/.bin/jasmine-node ./spec/lib/utils.spec.js
+
+clean:
+	rm -rf ./tmp/* ./public/uploads/*
--- tasks/clean.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(grunt){
-	grunt.registerTask('clean:uploads', "Cleans up uploaded files", function(){
-		var sh = require('shelljs');
-		sh.rm('-rf', './tmp/*', './public/uploads/*');
-		grunt.log.writeln('Deleting file uploads.');
-	});
-};
