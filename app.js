07a3e91cd3b36fa8829d28e468464b6464740b3a
richard-to <gohn76@gmail.com>
2012-11-10 12:49:10 -0900

    Add jpegtran optimization jpeg

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var server = function(config){
     var fs = require('fs');
     var async = require('async');
     var spawn = require('child_process').spawn;
-
+    var utils = require('./lib/utils');
     var app = express();
 
     app.engine('html', require('ejs').renderFile);
@@ -26,47 +26,62 @@ var server = function(config){
 
     app.post('/uploads/process', uploadMiddleware, function(req, res){
         if(req.files.image){
-            var imgName = req.files.image.name;  
+            var imgType = req.files.image.type;
+            if(imgType != 'image/jpeg' && imgType != 'image/png') {
+                res.json(500, "Error!");
+                return;
+            }
+
+            var imgName = req.files.image.name;
             var uploadDir = req.files.image.path.substring((__dirname + "/tmp/").length) + "/";
             var uploadUrlPath = '/uploads/' + uploadDir;
             var uploadFilePath = __dirname + '/public' + uploadUrlPath;
             
-            var output = uploadFilePath + "90_" + imgName;
-
+            var outFileNameJpg = utils.replaceFileExt(imgName, 'jpg');
+            var outFileNamePng = utils.replaceFileExt(imgName, 'png');
+            var outFilePathJpg = uploadFilePath + outFileNameJpg;
+            var outFilePathPng = uploadFilePath + outFileNamePng;
+            
             sh.mkdir('-p', uploadFilePath);
             sh.cp('-f', req.files.image.path, uploadFilePath + imgName);
 
-            var result = spawn('pngout', [uploadFilePath + imgName, output])
+            var result = spawn('pngout', [uploadFilePath + imgName, outFilePathPng]);
             result.on('exit', function(code){
-                var funcs = [];
-                var image_quality = [];
-                for(var i = 10; i > 1; --i){
-                    funcs.push((function(i){ 
-                        return function(callback){
-                            var quality = i*10;
-                            var new_file =  quality + "_test.jpg";
-                            image_quality.push({"image": uploadUrlPath + new_file});
-                            gm(output).quality(quality).write(uploadFilePath + new_file, function(err){
-                                if (!err) {
-                                    fs.stat(uploadFilePath + new_file, function(err, stats){
-                                        callback(null, {
-                                            image: uploadUrlPath + new_file,
-                                            size: Math.round(stats.size / 1000)
-                                        });
+                gm(outFilePathPng).write(outFilePathJpg, function(err){
+                    var result = spawn('jpegtran', ['-o', '-copy', 'none', 
+                        '-outfile', outFilePathJpg, outFilePathJpg]);
+                    result.on('exit', function(code){
+                        console.log(code);
+                        var funcs = [];
+                        var image_quality = [];
+                        for(var i = 10; i > 1; --i){
+                            funcs.push((function(i){ 
+                                return function(callback){
+                                    var quality = i*10;
+                                    var newFile =  quality + "_" + outFileNameJpg;
+                                    image_quality.push({"image": uploadUrlPath + newFile});
+                                    gm(outFilePathJpg).quality(quality).write(uploadFilePath + newFile, function(err){
+                                        if (!err) {
+                                            fs.stat(uploadFilePath + newFile, function(err, stats){
+                                                callback(null, {
+                                                    image: uploadUrlPath + newFile,
+                                                    size: Math.round(stats.size / 1000)
+                                                });
+                                            });
+                                        }
                                     });
-                                }
-                            });
+                                };
+                            })(i));
                         }
-                    })(i));
-                }
 
-                async.parallel(funcs, function(err, results){
-                    console.log(results);
-                    res.json({image: uploadUrlPath + imgName, optimized: results});
+                        async.parallel(funcs, function(err, results){
+                            res.json({image: uploadUrlPath + imgName, optimized: results});
+                        });
+                    });
                 });
             });
         } else {
-            res.json(500, "Error!")
+            res.json(500, "Error!");
         }
     });
 
