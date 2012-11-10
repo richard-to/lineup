ac6570dc979ae88aff15a5c233727f16ccc6ed65
richard-to <gohn76@gmail.com>
2012-11-09 23:29:50 -0900

    Add upload logic for optimizing image, then creating jpg versions of various quality

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var server = function(config){
     var _ = require('underscore');
     var express = require('express');
     var sh = require('shelljs');
+    var gm = require('gm');
+    var fs = require('fs');
+    var async = require('async');
+    var spawn = require('child_process').spawn;
 
     var app = express();
 
@@ -22,13 +26,45 @@ var server = function(config){
 
     app.post('/uploads/process', uploadMiddleware, function(req, res){
         if(req.files.image){
-            var uploadDir = __dirname + '/public/uploads/' + 
-                req.files.image.path.substring((__dirname + "/tmp/").length) + "/";
-            sh.mkdir('-p', uploadDir);
-            sh.cp('-f', req.files.image.path, uploadDir + req.files.image.name);
-            res.json({ image: '/uploads/' + 
-                req.files.image.path.substring((__dirname + "/tmp/").length) 
-                + "/" + req.files.image.name });
+            var imgName = req.files.image.name;  
+            var uploadDir = req.files.image.path.substring((__dirname + "/tmp/").length) + "/";
+            var uploadUrlPath = '/uploads/' + uploadDir;
+            var uploadFilePath = __dirname + '/public' + uploadUrlPath;
+            
+            var output = uploadFilePath + "90_" + imgName;
+
+            sh.mkdir('-p', uploadFilePath);
+            sh.cp('-f', req.files.image.path, uploadFilePath + imgName);
+
+            var result = spawn('pngout', [uploadFilePath + imgName, output])
+            result.on('exit', function(code){
+                var funcs = [];
+                var image_quality = [];
+                for(var i = 10; i > 1; --i){
+                    funcs.push((function(i){ 
+                        return function(callback){
+                            var quality = i*10;
+                            var new_file =  quality + "_test.jpg";
+                            image_quality.push({"image": uploadUrlPath + new_file});
+                            gm(output).quality(quality).write(uploadFilePath + new_file, function(err){
+                                if (!err) {
+                                    fs.stat(uploadFilePath + new_file, function(err, stats){
+                                        callback(null, {
+                                            image: uploadUrlPath + new_file,
+                                            size: Math.round(stats.size / 1000)
+                                        });
+                                    });
+                                }
+                            });
+                        }
+                    })(i));
+                }
+
+                async.parallel(funcs, function(err, results){
+                    console.log(results);
+                    res.json({image: uploadUrlPath + imgName, optimized: results});
+                });
+            });
         } else {
             res.json(500, "Error!")
         }
